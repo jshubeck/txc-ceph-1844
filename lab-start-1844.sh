3da3a30e2830e5e8016c2c8a49a206b5c4f54b47
John Shubeck <jrshubeck@gmail.com>
2026-08-06 08:49:27 -0700

    bug in rgw deploy

--- lab-start-1844.sh
+++ lab-start-1844.sh
@@ -18,7 +18,7 @@ ssh -q ceph-node1 sudo ceph orch apply osd --all-available-devices
 # Launch the RGW services
 echo "Starting the RADOS Gateway (RGW) daemon on Node 3 and Node 4 . . . "
 sleep 20
-ssh -q ceph-node1 sudo ceph orch apply rgw-s3service --placement "label:rgw"
+ssh -q ceph-node1 sudo ceph orch apply rgw s3service --placement "label:rgw"
 
 # Creating RCLONE configuration for IBM Cloud
 echo "Creating RCLONE configuration profile for IBM Cloud . . ."
